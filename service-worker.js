4e902a5cb568bfd0347cb99b29bb267a93df4e30
sebastiendesouza-tech <sebastiendesouza@free.fr>
2026-06-15 21:42:36 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caisse-by-sds-touche-21';
+const CACHE_NAME = 'caisse-by-sds-v2026-03';
 const FILES = ["./", "index.html", "style.css", "app.js", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", event => {
