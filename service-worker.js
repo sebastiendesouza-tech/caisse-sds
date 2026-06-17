13759c75096befca13c993584fdf90a0319e0a69
sebastiendesouza-tech <sebastiendesouza@free.fr>
2026-06-17 19:13:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caisse-manif-v01.3';
+const CACHE_NAME = 'caisse-manif-v26.18';
 const FILES = ["./", "index.html", "style.css", "app.js", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", event => {
